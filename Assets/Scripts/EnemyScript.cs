64b0b38ac3099c1b3b7109ac5f373989f23e9e6a
Amir Eldor <amir.eldor@gmail.com>
2015-08-16 09:02:31 +0300

    Initial commit, ass and lawnmower and enemies idea

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	private Vector3 rotationVector;
+	private float rotation_speed;
+	private float left, top, right, bottom;
+	private float speed;
+	private Sprite final_sprite;
+	private bool shitted;
+
+	// Use this for initialization
+	void Start () {
+		shitted = false;
+	}
+
+	public void randomRotationVector() {
+		rotationVector = new Vector3(0, 0, (-rotation_speed/2) + (Random.value * rotation_speed));
+		//rotationVector = new Vector3 (0, 0, 50);
+	}
+
+	/// <summary>
+	/// Changes the look, i.e. change the image that appears on the enemy:
+	/// roach, fly, etc...
+	/// </summary>
+	/// <param name="level">Level.</param>
+	public void Initialize(int level, float rotation_speed_in, float speed_in, float left_in, float top_in, float right_in, float bottom_in) {
+		Sprite[] sprites = Resources.LoadAll<Sprite> ("enemies");
+		SpriteRenderer sprite_renderer = GetComponent<SpriteRenderer> ();
+		level = Mathf.Clamp (level, 0, sprites.Length - 1);
+		sprite_renderer.sprite = sprites [level];
+		final_sprite = sprites [level + 10]; // pooped-upon sprite
+
+		rotation_speed = rotation_speed_in;
+		speed = speed_in;
+		left = left_in;
+		top = top_in;
+		right = right_in;
+		bottom = bottom_in;
+	}
+
+	/// <summary>
+	/// Should be called when shit hits the enemy
+	/// </summary>
+	public void Shitted() {
+		SpriteRenderer sprite_renderer = GetComponent<SpriteRenderer> ();
+		sprite_renderer.sprite = final_sprite;
+		shitted = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate () {
+		if (!shitted) {
+			transform.Rotate (rotationVector * Time.deltaTime);
+			transform.position += transform.up * speed * Time.deltaTime;
+
+			Vector3 position = transform.position;
+
+			// wrap around world
+			if (position.x < left) {
+				position.x = right;
+			}
+			if (position.x > right) {
+				position.x = left;
+			}
+			if (position.y < top) {
+				position.y = bottom;
+			}
+			if (position.y > bottom) {
+				position.y = top;
+			}
+			transform.position = position;
+		}
+	}
+
+	void MowedByLawnMower() {
+		if (shitted) {
+			// TODO add score!
+			//GetComponent<Renderer>().enabled = false;
+			Destroy (this.gameObject);
+		}
+	}
+
+	public bool IsShitted() {
+		return shitted;
+	}
+}	
---
+++ Assets/Scripts/MasterScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterScript : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	public float left, top, right, bottom;
+	public float enemy_rotation_speed;
+	public float enemy_speed;
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i<10;i++) {
+			GameObject obj = Instantiate(enemyPrefab, new Vector3(-2 + Random.value* 4, -2 + Random.value * 4, 0), Quaternion.identity) as GameObject;
+
+			// call Initialize
+			obj.GetComponent<EnemyScript>().Initialize(0, enemy_rotation_speed, enemy_speed, left, top, right, bottom);
+			obj.transform.Rotate(new Vector3(0, 0, Random.value * 360));
+		}
+
+		InvokeRepeating ("RotateEnemies", 0, 0.02f);
+	}
+
+	/// <summary>
+	/// Iterates through enemies and changes their rotationVector,
+	/// for an awesome wiggly moving effect.
+	/// </summary>
+	void RotateEnemies() {
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		foreach (GameObject enemy in enemies) {
+			EnemyScript script = enemy.GetComponent<EnemyScript>();
+			if (script) { // for some reason it was null on game very start, so ignore if null
+				script.randomRotationVector();
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
