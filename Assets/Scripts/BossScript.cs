37e709cdf6e42ed0a2abf5555ea0b19f72b3e47c
Amir Eldor <amir.eldor@gmail.com>
2015-08-17 18:45:59 +0300

    Boss moving around perfectly! Looks beautiful.

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -10,10 +10,12 @@ public class BossScript : MonoBehaviour
     private StateBehavior behavior;
 
     public float rotation_speed_normal = 30.0f;
-    public float rotation_speed_fire =360.0f;
+    public float rotation_speed_fire = 360.0f;
+    public float goto_speed = 0.8f;
     public Vector3 sway_vector = new Vector3(-0.3f, -4, 0); // boss sways stupidly when idle
 
-    private Vector3 target_vector; // go to this rotation at rotation speed
+    private Vector3 target_vector; // rotate to face this
+    private Vector3 goto_vector; // go to this position
     private float rotation_speed;
 
     // Use this for initialization
@@ -24,21 +26,20 @@ public class BossScript : MonoBehaviour
         new_pos.y = InterestingGameStuff.top + (InterestingGameStuff.bottom - InterestingGameStuff.top) / 2.0f;
         new_pos.z = 0;
         transform.position = new_pos;
+        goto_vector = new_pos;
 
         rotation_speed = rotation_speed_normal;
         target_vector = sway_vector;
+        // probably use time values in following Invoking in some setting or public variable
         InvokeRepeating("SwitchHappyToRotation", 0.6f, 0.6f);
-        behavior = RotateHappily;
+        InvokeRepeating("RandomizeGoTo", 1.4f, 1.4f);
+        behavior += RotateHappily;
+        behavior += JumpToGoTo;
     }
 
-    void BasicBehavior()
+    void JumpToGoTo()
     {
-        transform.Rotate(new Vector3(0, 0, 90 * Time.fixedDeltaTime));
-        if (transform.rotation.eulerAngles.z > 120.0f)
-        {
-            behavior -= BasicBehavior;
-            behavior += TestBehavior;
-        }
+        transform.position = Vector3.Slerp(transform.position, goto_vector, goto_speed * Time.fixedDeltaTime);
     }
 
     void DoRotation()
@@ -47,6 +48,17 @@ public class BossScript : MonoBehaviour
         Quaternion new_rotation = Quaternion.LookRotation(target - transform.position, Vector3.forward);
         new_rotation.x = new_rotation.y = 0;
         transform.rotation = Quaternion.Slerp(transform.rotation, new_rotation, rotation_speed * Time.fixedDeltaTime);
+    }
+
+    void RandomizeGoTo()
+    {
+        float left, top, right, bottom; // for easy shit (HA!)
+        left = InterestingGameStuff.left;
+        top = InterestingGameStuff.top;
+        right = InterestingGameStuff.right;
+        bottom = InterestingGameStuff.bottom;
+
+        goto_vector = new Vector3(right - ((right - left) * 0.2f), top + Random.value * (bottom - top), 0);
     }
 
     // switch the to_rotation, not the happiness into a rotation
