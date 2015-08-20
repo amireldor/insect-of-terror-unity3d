6964a194b121f59698da59e6cb69d49cec4e163d
Amir Eldor <amir.eldor@gmail.com>
2015-08-20 16:51:58 +0300

    Adding explosion prefab

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+
+    public float time = 0.8f;
+    public float rotation_speed = -294.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("EndLife", time);
+    }
+
+    void FixedUpdate() {
+        transform.Rotate(new Vector3(0, 0, 1) * rotation_speed * Time.fixedDeltaTime);
+	}
+
+    void EndLife()
+    {
+        Destroy(this.gameObject);
+    }
+}
